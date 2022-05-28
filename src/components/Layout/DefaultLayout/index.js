b420cdd4371ea928adf878a8f834894af34e719d
Haitran11202 <haitran11202@gmail.com>
2022-05-28 10:52:56 +0700

    Thêm components trong file Layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,5 +1,5 @@
 import { Children } from 'react';
-import Header from './Header';
+import Header from '~/components/Layout/components/Header';
 import SideBar from './SideBar';
 
 function DefaultLayout({ children }) {
--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,5 +1,5 @@
 import { Children } from 'react';
-import Header from './Header';
+import Header from '~/components/Layout/components/Header';
 
 function HeaderOnly({ children }) {
   return (
